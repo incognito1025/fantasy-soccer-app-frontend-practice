2ffa8d94d0161ad3ad165d8f369f9866c08a2b7f
incognito1025 <nkhanamart@gmail.com>
2024-07-30 06:08:19 -0400

    drafted NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,47 @@
+import { Link } from 'react-router-dom'
+import "./NavBar.css"
+
 export default function NavBar() {
     return (
-        <div className="NavBar">
-            <h1>This is the NavBar!</h1>
-        </div>
+        // <div className="NavBar">
+        //     <h1>This is the NavBar!</h1>
+        // </div>
+        <nav>
+
+            <Link to='/'>
+                <p>Login</p>
+            </Link>
+
+            <Link to='/home'>
+                <p>
+                    Home<img src='src/assets/fs-logo.png' alt="logo" className="logo" />
+                </p>
+            </Link>
+
+            <Link to='/players'>
+                <p>Player Directory</p>
+            </Link>
+
+            <Link to='/players/:new'>
+                <p>Create Your Player!</p>
+            </Link>
+
+            <Link to='players/add'>
+                <p>Create Your Team! </p>
+            </Link>
+
+            <Link to='/players/:id'>
+                <p>Search for Player</p>
+            </Link>
+
+            <Link to='/players/:id/edit'>
+                <p>Edit Player/Team</p>
+            </Link>
+
+            <Link to='/about'>
+                <p>About</p>
+            </Link>
+
+        </nav>
     )
 }
